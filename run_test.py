0c047658003f7db703d208be73eccac2ee3cea5c
Gabriele Martinelli <marti.gabi11@gmail.com>
2026-04-23 13:07:14 +0200

    remove obsolete test and utility scripts from lakegen_demo directory

--- run_test.py
+++
@@ -1,6 +0,0 @@
-import pty
-import os
-import sys
-
-# Just run the main script and feed it a simple query
-# Actually, I can just use pexpect or standard input redirection if main.py accepts stdin.
--- test_llm_event.py
+++
@@ -1,2 +0,0 @@
-from llama_index.core.instrumentation.events.llm import LLMChatEndEvent
-print(dir(LLMChatEndEvent))
